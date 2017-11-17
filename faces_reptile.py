3dc7ea1c197fe56fe0261b12cd828dba1fe52a82
HanxiongXiao <296241332@qq.com>
2017-11-17 22:06:56 +0800

    临时提交

--- faces_reptile.py
+++ faces_reptile.py
@@ -2,29 +2,34 @@
 import threading
 import time
 import queue
+import urllib.request
 
 class FacesReptile(threading.Thread):
-    def __init__(self, thread_id, name, counter):
+    def __init__(self, thread_id, que_idx, que_und, que_liner):
         threading.Thread.__init__(self)
         self.thread_id = thread_id
-        self.name = name
-        self.counter = counter
+        self.que_idx = que_idx
+        self.que_und = que_und
+        self.que_liner = que_liner
 
     def run(self):
-        print "Starting " + self.name
-       # 获得锁，成功获得锁定后返回True
-       # 可选的timeout参数不填时将一直阻塞直到获得锁定
-       # 否则超时后将返回False
-        threadLock.acquire()
-        print_time(self.name, self.counter, 3)
-        # 释放锁
-        threadLock.release()
+        while not self.que_liner.empty():
+            liner = self.que_liner.get_nowait()
+            self.download(liner)
 
-def print_time(threadName, delay, counter):
-    while counter:
-        time.sleep(delay)
-        print "%s: %s" % (threadName, time.ctime(time.time()))
-        counter -= 1
+    def download(self, liner):
+        content = liner.split('\t')
+        name = content[0].replace(' ', '_')
+        img_id = content[1]
+        face_id = content[2]
+        url = content[3]
+        bbox = content[4]
+
+        try:
+            urllib.request.urlopen('http://upload.wikimedia.org/wikipedia/commons/5/5d/AaronEckhart10TIFF.jpg')
+            urllib.urlretrieve()
+        except:
+            pass
 
 
 def get_information_queue(path, type, num_thread):
@@ -43,57 +48,49 @@ def get_information_queue(path, type, num_thread):
         queue_list.append(queue.Queue())
         i = 0
         while i < counter and not liner_que.empty():
-            queue_list[n].put(liner_que.get())
-            i += 0
-
-
+            queue_list[n].put_nowait(liner_que.get_nowait()) # no wait
+            i += 1
+    # return queue list
+    return queue_list
 
 
 def grab_faces(path, type, num_thread):
     src_file = open('{}/facescrub_{}.txt'.format(path, type), 'r')
     idx_file = open('{}/record/facescrub_idx_{}.txt'.format(path, type), 'w')
-    liner_que = queue.deque()
+    und_file = open('')
 
-    # construct liner queue
-    for liner in src_file:
-        content = liner.split('\t')
-        liner_que.append(content)
-        # print(content)
+    queue_list = get_information_queue('H:\DATA\FaceScrub', 'actors', 4)
+    assert len(queue_list) == num_thread
 
-    # create faces reptile
+    queue_list_idx = queue.Queue()
+    queue_list_und = queue.Queue()
+
+    # create faces reptiles
     for n in range(num_thread):
-        reptile = FacesReptile()
+        reptile = FacesReptile(n, queue_list_idx, queue_list_und, queue_list[n])
+        reptile.start()
+
+    # for
 
 
 
 
 
 if __name__ == '__main__':
-    get_information_queue('H:\DATA\FaceScrub', 'actors', 4)
+    # get_information_queue('H:\DATA\FaceScrub', 'actors', 4)
+    # data = urllib.request.urlopen('http://upload.wikimedia.org/wikipedia/commons/5/5d/AaronEckhart10TIFF2.jpg')
+    # data = urllib.request.urlopen('http://www.google.com.hk')
+    # print(data.read())
 
+    url = 'http://upload.wikimedia.org/wikipedia/commons/5/5d/AaronEckhart10TIFF.jpg'
+    urllib.urlretrieve(url, 'tmp.jpg')
+    try:
+        data = urllib.request.urlopen(url)
+        urllib.urlretrieve(url, 'tmp.jpg')
+        print(data)
+        print('success')
+    except:
+        print(data)
+        print('fail')
 
-
-    # src_file_actors = open('facescrub_actors.txt', 'r')
-    # src_file_actresses = open('facescrub_actresses.txt', 'r')
-    #
-    #
-    #
-    # threadLock = threading.Lock()
-    # threads = []
-    #
-    # # 创建新线程
-    # thread1 = myThread(1, "Thread-1", 1)
-    # thread2 = myThread(2, "Thread-2", 2)
-    #
-    # # 开启新线程
-    # thread1.start()
-    # thread2.start()
-    #
-    # # 添加线程到线程列表
-    # threads.append(thread1)
-    # threads.append(thread2)
-    #
-    # # 等待所有线程完成
-    # for t in threads:
-    #     t.join()
 
